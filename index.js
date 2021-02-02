6348420defb4e8a8f2a8b3bc869f8ceb11bdf131
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-02-02 11:33:02 -0500

    nodemon, mongoose, dotenv, cors

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
+const { dbConexion } = require('./database/config');
 
 // creamo e iniciamos el servidor de express
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Express corriendo en el puerto 3000');
+// Base de datos
+dbConexion();
+
+// Cors: no olvidar configurar la whitelist
+app.use(cors());
+
+// Rutas
+app.get('/', (req, resp) => {
+  resp.status(200).json({
+    isSuccess: true,
+    data: 'ok',
+    message: 'Solicitud realizada con éxito',
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Express corriendo en el puerto ' + process.env.PORT);
 });
